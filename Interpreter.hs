2a29b1cf6d32932b203a0afaff88c92ff6c71851
John Collier <jmcollier139@gmail.com>
2015-03-21 12:09:34 -0400

    List lookup

--- Interpreter.hs
+++ Interpreter.hs
@@ -42,7 +42,7 @@ getMaybe (Just val) = val
 
 -- Helper function that searches for an identifier and returns its corresponding value
 getIdentifier :: BaseExpr -> [(BaseExpr, Expr)] -> Expr
-getIdentifier (Atom ident) exprs = (getMaybe (lookup ident exprs))
+getIdentifier ident exprs = (getMaybe (lookup ident exprs))
 
 -- |Take the output of the base parser and interpret it,
 --  first constructing the AST, then evaluating it,
