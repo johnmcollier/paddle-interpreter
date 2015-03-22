13522de85be22de6971abf6e98159326fa49f668
Collier John Matthew <g3collie@cdf.toronto.edu>
2015-03-22 18:57:42 -0400

    Major progress towards identifiers

--- Interpreter.hs
+++ Interpreter.hs
@@ -44,17 +44,32 @@ getMaybe (Just val) = val
 getIdentifier :: [Char] -> [([Char], Expr)] -> Expr
 getIdentifier ident expr = (getMaybe (lookup ident expr))
 
+-- Inserts a new identifier into the identifier list and returns the updated list
+putIdentifier :: ([Char], Expr) -> [([Char], Expr)] -> [([Char], Expr)]
+putIdentifier (name, expr) idents = (name, expr):idents
+
+isEmpty :: [([Char], Expr)] -> Bool
+isEmpty idents = null idents
+
 -- |Take the output of the base parser and interpret it,
 --  first constructing the AST, then evaluating it,
 --  and finally returning string representations of the results.
 --  You will need to make this function more robust against errors.
 interpretPaddle :: Maybe [BaseExpr] -> String
 interpretPaddle (Just exprs) =
-    let ast = map parseExpr exprs
-        vals = map evaluate ast
+    let ast = map parseExpr exprs 
+        vals = evalExpr ast []
     in
         -- String representations of each value, joined with newlines
         unlines (map show vals)
+
+evalExpr :: [Expr] -> [([Char], Expr)] -> [Expr]
+evalExpr asts idens = 
+	if ((length asts) == 1)
+		then let (_, output) = (evaluate idens (head asts))
+		in output:[]
+		else let (n_idens, output) = (evaluate idens (head asts))
+		in output:(evalExpr (tail asts) n_idens)
 
 -- An expression data type
 data Expr = Number Integer |
@@ -62,7 +77,9 @@ data Expr = Number Integer |
             If Expr Expr Expr |
             Not Expr |
             List [Expr] |
-            BinaryExpr BaseExpr Expr Expr
+            BinaryExpr BaseExpr Expr Expr |
+			Identifier [Char] Expr |
+			Error
             
 instance Show Expr where
     show (Number x) = show x
@@ -70,7 +87,7 @@ instance Show Expr where
     show (Boolean False) = "#f"
     show (List []) = "'()"
     show (List vals) = (printList vals "(")
-
+    show (Identifier _ expr) = show expr
     -- Note: the following definition is not necessary for this assignment,
     -- but you may find it helpful to define string representations of all
     -- expression forms.
@@ -92,6 +109,8 @@ parseExpr (Compound ((Atom "list"):vals)) =
     List (map parseExpr vals)
 parseExpr (Compound [Atom "if", b, x, y]) =
     If (parseExpr b) (parseExpr x) (parseExpr y)
+parseExpr (Compound [Atom "define", Atom name, expr]) =
+	Identifier name (parseExpr expr)
 -- Parse any binary expression (an expression with two parameters)
 parseExpr (Compound [(Atom opStr), x, y]) =
     BinaryExpr (Atom opStr) (parseExpr x) (parseExpr y)
@@ -100,40 +119,44 @@ parseExpr (Compound [Atom "not", x]) =
     Not (parseExpr x)
 
 
-
-
 -- |Evaluate an AST by simplifying it into
 --  a number, boolean, list, or function value.
-evaluate :: Expr -> Expr
-evaluate (Number n) = Number n
-evaluate (Boolean b) = Boolean b
+evaluate :: [([Char], Expr)] -> Expr -> ([([Char], Expr)], Expr)
+evaluate idents (Number n) = (idents, (Number n))
+evaluate idents (Boolean b) = (idents, (Boolean b))
 
 -- If Statement expression. 
 -- Rewritten to allow subexpression for all three parameters
-evaluate (If (Boolean cond) x y) =
+evaluate ident (If (Boolean cond) x y) =
     case cond of
-        True -> (evaluate x)
-        False -> (evaluate y)
-evaluate (If cond x y) =
-    (evaluate (If (evaluate cond) x y))
+        True -> (evaluate ident x)
+        False -> (evaluate ident y)
+evaluate ident (If cond x y) =
+	let (_, output) = (evaluate ident cond)
+	in (evaluate ident (If output x y))
 
 -- Evaluate some kind of binary expression
-evaluate (BinaryExpr (Atom opStr) (Number x) (Number y)) =
+evaluate ident (BinaryExpr (Atom opStr) (Number x) (Number y)) =
     case opStr of
-        "+" -> (Number (x + y))
-        "*" -> (Number (x * y))
-        "equal?" -> (Boolean (x == y))
-evaluate (BinaryExpr (Atom opStr) (Boolean x) (Boolean y)) =
+        "+" -> (ident, (Number (x + y)))
+        "*" -> (ident, (Number (x * y)))
+        "equal?" -> (ident, (Boolean (x == y)))
+evaluate ident (BinaryExpr (Atom opStr) (Boolean x) (Boolean y)) =
    case opStr of
-       "and" -> (Boolean (x && y))
-       "or" -> (Boolean (x || y))
-       "equal?" -> (Boolean (x == y))
-evaluate (BinaryExpr (Atom opStr) x y) = 
-    (evaluate (BinaryExpr (Atom opStr) (evaluate x) (evaluate y)))
+       "and" -> (ident, (Boolean (x && y)))
+       "or" -> (ident, (Boolean (x || y)))
+       "equal?" -> (ident, (Boolean (x == y)))
+evaluate ident (BinaryExpr (Atom opStr) x y) =
+	let (identX, outputX) = (evaluate ident x)
+	in let (identY, outputY) = (evaluate ident y)
+	in (evaluate ident (BinaryExpr (Atom opStr) outputX outputY))
 
 -- Evaluate a 'not' expression
-evaluate (Not (Boolean x)) = (Boolean (not x))
-evaluate (Not x) = (evaluate (Not (evaluate x)))
+evaluate ident (Not (Boolean x)) = (ident, (Boolean (not x)))
+evaluate ident (Not x) = 
+	let (_, output) = (evaluate ident x)
+	in (evaluate ident (Not output))
 
 -- Evaluate a 'list'
-evaluate (List vals) = (List (map evaluate vals))
+evaluate ident (List vals) = 
+	(ident, (List (map (\x -> let (_, output) = (evaluate ident x) in output) vals)))
